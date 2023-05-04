6e7657fd7ba8302fa17d51ed3d704720cd257a8f
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-04 21:56:43 +0300

    add App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
+    </Container>
   );
 };
