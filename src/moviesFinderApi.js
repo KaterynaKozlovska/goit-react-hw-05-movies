094fc68decda70e2988eb53bdac723742ecbe447
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-05 23:08:04 +0300

    add api

--- src/moviesFinderApi.js
+++ src/moviesFinderApi.js
@@ -1,11 +1,40 @@
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
 const API_KEY = '23ef7ebe7a5765558b3c745e54a99f35';
-const BASE_URL = 'api.themoviedb.org';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 
-export default async function imageFinderApi(name, page) {
-  const response = await fetch(
-    `${BASE_URL}/?q=${name}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-  );
-
-  return response.ok;
+async function MoviesFinderApi(pathMovies) {
+  const axiosInstance = axios.create({
+    baseURL: `${BASE_URL}${pathMovies}`,
+    headers: { 'Content-Type': 'application/json' },
+    params: {
+      api_key: API_KEY,
+    },
+  });
+  return await axiosInstance.get();
 }
-// https://api.themoviedb.org/3/movie/550?api_key=23ef7ebe7a5765558b3c745e54a99f35
+
+async function MoviesFinderApiById(id, path) {
+  const pathURL = path ? `/${path}` : '';
+  const pathMovies = `movie/${id}${pathURL}`;
+  const axiosInstance = axios.create({
+    baseURL: `${BASE_URL}${pathMovies}`,
+    headers: { 'Content-Type': 'application/json' },
+    params: {
+      api_key: API_KEY,
+    },
+  });
+
+  return await axiosInstance.get();
+}
+MoviesFinderApi.propTypes = {
+  pathMovies: PropTypes.string.isRequired,
+};
+
+MoviesFinderApiById.propTypes = {
+  id: PropTypes.string.isRequired,
+  path: PropTypes.string,
+};
+
+export { MoviesFinderApi, MoviesFinderApiById };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,56 @@
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
+// axios.defaults.baseURL = 'https://api.themoviedb.org';
+// const MoviesListFinder = ({ movies }) => (
+//   <ul>
+//     {movies.map(({ objectID, url, title }) => (
+//       <li key={objectID}>
+//         <a href={url} target="_blank" rel="noreferrer noopener">
+//           {title}
+//         </a>
+//       </li>
+//     ))}
+//   </ul>
+// );
+
+// `https://api.themoviedb.org/3/movie/550?api_key=23ef7ebe7a5765558b3c745e54a99f35`;
 
 export const Home = () => {
-  // useEffect(() => {
-  //   `https://api.themoviedb.org/3/movie/550?api_key=23ef7ebe7a5765558b3c745e54a99f35`;
-  // }, []);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    try {
+      const response = axios.get(MoviesListFinder(movies));
+      setMovies(prevState => [...prevState, response]);
+    } catch {
+    } finally {
+      setIsLoading(false);
+    }
+  });
+
   return (
     <main>
-      <h1>Welcome</h1>
+      <div>
+        {isLoading} <p>Loading...</p> <MoviesListFinder movies={movies} /> :
+        null
+      </div>
     </main>
   );
 };
+
+//    componentDidMount()
+//     this.setState({ isLoading: true });
+//     const response = await axios.get("/search?query=react");
+//     this.setState({ movies: response.data.hits, isLoading: false, });
+//   }
+
+//   render()
+//     const { movies, isLoading  } = this.state;
+
+//
