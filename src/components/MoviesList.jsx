c09b394179e566c7b263e9d73033979e70b8944c
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-07 16:54:24 +0300

    Update MoviesList.jsx

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,11 +1,13 @@
 import { List, Item, Image, StyledLink, Name } from './MoviesList.styled';
+import { useLocation } from 'react-router-dom';
 
 export const MoviesList = ({ movies }) => {
+  const location = useLocation();
   return (
     <List>
       {movies.map(({ id, title, poster_path }) => (
         <Item key={id}>
-          <StyledLink to={`/movies/&{id}`}>
+          <StyledLink to={`/movies/&{id}`} state={{ from: location }}>
             <Image
               src={
                 poster_path
@@ -15,6 +17,7 @@ export const MoviesList = ({ movies }) => {
               alt={title}
               width="100"
               height="160"
+              loading="lazy"
             />
             <Name>{title}</Name>
           </StyledLink>
